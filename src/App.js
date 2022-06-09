20a0f63223b08d9a35bc80151e2fc1433005871f
jacobnathanson <jacobnath93@gmail.com>
2022-06-09 10:21:12 -0400

    basic barebone layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@
 function App() {
   return (
     <div className="App">
-    
+
+      <div className="border">title</div>
+      <div className="border">winners lounge</div>
+      <div className="border">current match</div>
+      <div className="border">waiting lobby</div>
+      <div className="border">form</div>
+
     </div>
   );
 }
